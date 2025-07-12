ed4f45c965a06c43a194d916a2346f58fed242fc
Vivek-Gokhale <vivekgokhale23@gmail.com>
2025-07-12 10:12:44 +0530

    auth feature backend implmented

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
+const userRoutes = require('./userRoutes'); // Assuming userRoutes is defined in userRoutes.js
+router.use('/users', userRoutes);
 module.exports = router;
